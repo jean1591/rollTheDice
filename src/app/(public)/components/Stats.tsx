ce6897eb608517a049b3641bb10d02b53bd4cd1b
Jean Robertou <j-rob@live.fr>
2024-08-16 18:58:59 +0200

    chore: use grid instead of table

--- src/app/(public)/components/Stats.tsx
+++ src/app/(public)/components/Stats.tsx
@@ -10,45 +10,24 @@ export const Stats = () => {
     <div className="rounded-lg p-4 shadow-lg">
       <p className="text-left text-xl font-semibold">Live stats</p>
 
-      <table className="min-w-full divide-y divide-slate-300">
-        <thead>
-          <tr>
-            <th
-              scope="col"
-              className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold sm:pl-0"
-            >
-              Value
-            </th>
-            <th
-              scope="col"
-              className="px-3 py-3.5 text-left text-sm font-semibold"
-            >
-              Count
-            </th>
-            <th
-              scope="col"
-              className="px-3 py-3.5 text-left text-sm font-semibold"
-            >
-              %
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-slate-200">
-          {Object.entries(rolls).map(([key, value]) => (
-            <tr key={key}>
-              <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-slate-500 sm:pl-0">
-                {key}
-              </td>
-              <td className="whitespace-nowrap px-3 py-2 text-sm font-semibold">
-                {value.count}
-              </td>
-              <td className="whitespace-nowrap px-3 py-2 text-sm">
-                {value.percentage} %
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="grid grid-cols-4 gap-x-2 text-sm">
+        <p className="col-span-2 py-4 text-left font-semibold">Value</p>
+        <p className="py-4 text-left font-semibold">Count</p>
+        <p className="py-4 text-left font-semibold">%</p>
+      </div>
+
+      {Object.entries(rolls).map(([key, value]) => (
+        <div
+          key={key}
+          className="grid grid-cols-4 gap-x-2 border-t-[1px] border-slate-300 text-sm"
+        >
+          <p className="col-span-2 py-2 text-sm text-slate-500 sm:pl-0">
+            {key}
+          </p>
+          <p className="py-2 text-sm font-semibold">{value.count}</p>
+          <p className="py-2 text-sm">{value.percentage} %</p>
+        </div>
+      ))}
     </div>
   )
 }
