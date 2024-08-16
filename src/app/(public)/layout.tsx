863d8efd107c9d7e77fef8ffa14b01cd51b5d0d1
Jean Robertou <j-rob@live.fr>
2024-08-16 17:35:37 +0200

    chore: add padding to md screens

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({
   return (
     <div className="flex min-h-screen flex-col">
       <div className="flex-grow">
-        <div className="mx-auto my-20 flex max-w-5xl flex-col items-center justify-center px-4 md:px-0">
+        <div className="mx-auto my-20 flex max-w-5xl flex-col items-center justify-center px-4 lg:px-0">
           {children}
         </div>
       </div>
